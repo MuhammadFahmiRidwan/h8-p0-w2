7e735a0b551d74a89799bd47bdab1026529d7ccf
MuhammadFahmiRidwan <mfahmiridwan50@gmail.com>
2019-07-30 20:41:16 +0700

    first

--- Selasa-Dunia-JavaScript/exercise-3.js
+++ Selasa-Dunia-JavaScript/exercise-3.js
@@ -1,52 +1,21 @@
-var tanggal = 12
-var bulan = 11
-var tahun = 1996
+var nama = "Fahmi"
+var peran = "Penyihir"
 
-if (tanggal < 1 || tanggal > 31) {
-    console.log('Masukkan tanggal dengan angka antara 1-31');  
+if (nama == "" && peran == "") {
+    console.log("Nama harus diisi!")
 }
-else if (bulan < 1 || bulan > 12) {
-    console.log('Masukkan bulan dengan angka antara 1-12');
+else if (peran == "") {
+    console.log("Halo " + nama + ",Pilih peranmu untuk memulai game!")
 }
-else if (tahun < 1900 || tahun > 2200) {
-    console.log('Masukkan tahun dengan angka antara 1900-2200');
+else if (peran === "Ksatria") {
+    console.log ("Selamat datang di Dunia Proxytia," + nama)
+    console.log ("Halo " + peran + " " + nama + ", kamu dapat menyerang dengan senjatamu!")
 }
-else {
-switch (bulan) {
-    case 1 :
-        console.log(tanggal + ' Januari ' + tahun);
-    break;
-    case 2:
-        console.log(tanggal + ' Februari ' + tahun);
-    break;
-    case 3:
-        console.log(tanggal + ' Maret ' + tahun);
-    break;
-    case 4:
-        console.log(tanggal + ' April ' + tahun);
-    break;
-    case 5 :
-        console.log(tanggal + ' Mei ' + tahun);
-    break;
-    case 6:
-        console.log(tanggal + ' Juni ' + tahun);
-    break;
-    case 7:
-        console.log(tanggal + ' Juli ' + tahun);
-    break;
-    case 8:
-        console.log(tanggal + ' Agustus ' + tahun);
-    break;
-    case 9:
-        console.log(tanggal + ' September ' + tahun);
-    break;
-    case 10:
-        console.log(tanggal + ' Oktober ' + tahun);
-    break;
-    case 11:
-        console.log(tanggal + ' November ' + tahun);
-    break;
-    case 12:
-        console.log(tanggal + ' Desember ' + tahun);
+else if (peran === "Tabib") {
+    console.log ("Selamat datang di Dunia Proxytia," + nama)
+    console.log ("Halo " +peran + " " + nama + " kamu akan membantu temanmu yang terluka.")
 }
+else if (peran === "Penyihir") {
+    console.log ("Selamat datang di Dunia Proxytia," + nama)
+    console.log ("Halo " + peran + " " + nama + ",ciptakan keajaiban yang membantu kemenanganmu!")
 }
