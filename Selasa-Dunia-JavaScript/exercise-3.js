efffce713e4a2259ec298da8b6a29ace053fb568
MuhammadFahmiRidwan <mfahmiridwan50@gmail.com>
2019-11-28 14:22:56 +0700

    first

--- Selasa-Dunia-JavaScript/exercise-3.js
+++ Selasa-Dunia-JavaScript/exercise-3.js
@@ -1,21 +1,23 @@
 var nama = "Fahmi"
-var peran = "Penyihir"
+var peran = ""
 
 if (nama == false) {
     console.log("Nama harus diisi!")
 }
-else if (peran == false) {
-    console.log("Halo " + nama + ",Pilih peranmu untuk memulai game!")
-}
-else if (peran === "Ksatria") {
-    console.log ("Selamat datang di Dunia Proxytia," + nama)
-    console.log ("Halo " + peran + " " + nama + ", kamu dapat menyerang dengan senjatamu!")
-}
-else if (peran === "Tabib") {
-    console.log ("Selamat datang di Dunia Proxytia," + nama)
-    console.log ("Halo " +peran + " " + nama + " kamu akan membantu temanmu yang terluka.")
-}
-else if (peran === "Penyihir") {
-    console.log ("Selamat datang di Dunia Proxytia," + nama)
-    console.log ("Halo " + peran + " " + nama + ",ciptakan keajaiban yang membantu kemenanganmu!")
+else {
+    if (peran === "Ksatria") {
+        console.log ("Selamat datang di Dunia Proxytia," + nama)
+        console.log ("Halo " + peran + " " + nama + ", kamu dapat menyerang dengan senjatamu!")
+    }
+    else if (peran === "Tabib") {
+        console.log ("Selamat datang di Dunia Proxytia," + nama)
+        console.log ("Halo " +peran + " " + nama + " kamu akan membantu temanmu yang terluka.")
+    }
+    else if (peran === "Penyihir") {
+        console.log ("Selamat datang di Dunia Proxytia," + nama)
+        console.log ("Halo " + peran + " " + nama + ",ciptakan keajaiban yang membantu kemenanganmu!")
+    }
+    else{
+        console.log("Halo " + nama + ",Pilih peranmu untuk memulai game!")
+    }
 }
